35da5e8ec4393ec83ea1ba226326b99eb5e9119d
Minhaj Mehmood <minhaj.kk@gmail.com>
2019-04-06 15:04:17 +0400

    Now password is hashed

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const passport = require('passport');
+const bcrypt = require('bcryptjs');
 const User = require('./models/User');
 const app = express();
 
@@ -14,6 +16,10 @@ mongoose
     .then(() => console.log('MongoDB Connected'))
     .catch(err => console.log(err));
 
+// Init passport
+app.use(passport.initialize());
+require('./config/passport')(passport);
+
 /* GET home page. */
 app.get('/', (req,res) => res.json({
 	msg: "Hello! Welcome to BAWN(Backend API with NodeJS)"
@@ -21,16 +27,28 @@ app.get('/', (req,res) => res.json({
 
 app.post('/register', (req, res) => {
 
-	const newUser = new User({
-	    name: req.body.name,
-	    email: req.body.email,
-	    password: req.body.password
-	});
+    User.findOne({ email: req.body.email }).then(user => {
+        if (user) {
+            return res.status(400).json({ email: "Email already exists" });
+        } else {
+            const newUser = new User({
+                name: req.body.name,
+                email: req.body.email,
+                password: req.body.password
+            });
 
-	newUser
-	    .save()
-	    .then(user => res.json(user))
-	    .catch(err => res.json(err));
+            bcrypt.genSalt(10, (err, salt) => {
+                bcrypt.hash(newUser.password, salt, (err, hash) => {
+                    if (err) throw err;
+                    newUser.password = hash;
+                    newUser
+                        .save()
+                        .then(user => res.json(user))
+                        .catch(err => console.log(err));
+                });
+            });
+        }
+    });
 });
 
 const port = process.env.PORT || 5000;
